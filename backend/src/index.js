43af5b8efaf5575079ec2963ddd54f4068d9a50d
ArielSanchez12 <riveraariel433@gmail.com>
2025-06-01 18:46:27 -0500

    vuelta

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,9 @@
 import connection from './database.js'
 import app from './server.js'
 
+
 connection()
 
-// Elimina o comenta la línea de app.listen para producción en Vercel
-// app.listen(app.get('port'), () => { 
-//     console.log("Server ok")
-// })
-
-export default app // Esto es lo que Vercel necesita
+app.listen(app.get('port'), () => { //Esta escuchando a nuestra variable global 'port'
+    console.log("Server ok")
+})

04852d77e5fc9b2f549890bbfe19b14cf2869989
ArielSanchez12 <riveraariel433@gmail.com>
2025-06-03 20:46:23 -0500

    cambio en la conexion a mongo

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,13 +2,16 @@ import mongoose from 'mongoose'
 
 mongoose.set('strictQuery', true) //Esto quiere decir que si tenemos 3 campos, DEBEN PASARSE LOS 3 CAMPOS al insertar un registro, sino no se dejara insertar
 
-const connection = async () => { 
-    try {
-        await mongoose.connect(process.env.MONGODB_URI_LOCAL)
-        console.log("Database is connected")
-    } catch (error) {
-        console.log(error)
-    }
-}
+const connection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI_LOCAL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB conectado");
+  } catch (error) {
+    console.log("Error de conexión a MongoDB:", error.message);
+  }
+};
 
-export default connection
+export default connection;

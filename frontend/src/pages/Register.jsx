f5d33d2584b7a6839e8e969a0e216af623a17702
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-06-10 14:51:48 -0500

    logincito

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -89,18 +89,20 @@ export const Register = () => {
                             <label className="mb-2 block text-base font-semibold">Contraseña</label>
                             <div className="relative">
                                 <input
-                                    type={showPassword ? "text" : "password"}
+                                    type={showPassword ? "text" : "password"} // Cambia el tipo del input entre 'text' y 'password' según el estado
                                     placeholder="********************"
                                     className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500 pr-10"
                                     {...register("password", { required: "Este campo es obligatorio!" })}
                                 />
-                                    {errors.password && <p className="text-red-800">{errors.password.message}</p>}
+                                {errors.password && <p className="text-red-800">{errors.password.message}</p>}
                                     
+                                {/* Botón para mostrar/ocultar la contraseña */}
                                 <button
                                     type="button"
-                                    onClick={() => setShowPassword(!showPassword)}
+                                    onClick={() => setShowPassword(!showPassword)} // Cambia el estado para mostrar/ocultar la contraseña
                                     className="absolute top-2 right-3 text-gray-500 hover:text-gray-700"
                                 >
+                                    {/* Icono que cambia según el estado de la contraseña */}
                                     {showPassword ? (
                                         <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.875 18.825A9.956 9.956 0 0112 19c-4.418 0-8.165-2.928-9.53-7a10.005 10.005 0 0119.06 0 9.956 9.956 0 01-1.845 3.35M9.9 14.32a3 3 0 114.2-4.2m.5 3.5l3.8 3.8m-3.8-3.8L5.5 5.5" />

d96ee0c8af8425a4d19404839615893e3306316f
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-06-17 15:06:08 -0500

    bueno dashboard

--- frontend/src/layout/Dashboard.jsx
+++ frontend/src/layout/Dashboard.jsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
 
                 <h2 className='text-4xl font-black text-center text-slate-200'>LabTRACK</h2>
 
-                <img src="https://cdn-icons-png.flaticon.com/512/2138/2138508.png" alt="img-client" className="m-auto mt-8 p-1 border-2 border-slate-500 rounded-full" width={120} height={120} />
+                <img src="https://images.pexels.com/photos/32566238/pexels-photo-32566238.jpeg" alt="img-client" className="m-auto mt-8 p-1 border-2 border-slate-500 rounded-full" width={120} height={120} />
                 <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-green-600 w-3 h-3 inline-block rounded-full'></span> Bienvenido - </p>
                 <p className='text-slate-400 text-center my-4 text-sm'> Rol - </p>
                 <hr className="mt-5 border-slate-500" />

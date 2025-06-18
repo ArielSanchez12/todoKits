db51d216fa6804402d2d602488b554592fdfad49
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-06-18 15:48:47 -0500

    proteccion de token front

---
+++ frontend/src/context/storeAuth.jsx
@@ -0,0 +1,21 @@
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+
+
+const storeAuth = create(
+    // MIDDLEWARE
+    persist(
+        // iNFORMACION A COMPARTIR
+    set => ({
+        token: null,
+        rol:null,
+        setToken: (token) => set({ token }),
+        setRol: (rol) => set({ rol }),
+        clearToken: () => set({ token: null})
+    }),
+    // LOCALSTORAGE
+    { name: "auth-token" } 
+    )
+)
+
+export default storeAuth;

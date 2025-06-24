5fa11a5775ab64f8569848cc1b09dd17540f6631
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-06-24 15:30:31 -0500

    storeProfile y auth frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,27 +16,41 @@ import Reset from './pages/Reset'
 
 import PublicRoute from './routes/PublicRoute'
 import ProtectedRoute from './routes/ProtectedRoute'
+import { useEffect } from 'react'
+import storeProfile from './context/storeProfile'
+import storeAuth from './context/storeAuth'
 
 
 
 function App() {
+
+  const { profile} = storeProfile()
+  const { token } = storeAuth()
+
+  useEffect(() => {
+    if(token){
+      profile()
+    }
+  }, [token])
+
+
   return (
     <>
     <BrowserRouter>
       <Routes>
         
         <Route element={<PublicRoute />}>
-        <Route index element={<Login/>}/>
-        <Route path='login' element={<Login/>}/>
-        <Route path='register' element={<Register/>}/>
-        <Route path='forgot/:id' element={<Forgot/>}/>
-        <Route path='confirm/:token' element={<Confirm/>}/>
-        <Route path='reset/:token' element={<Reset/>}/>
-        <Route path='*' element={<NotFound />} />
+          <Route index element={<Login/>}/>
+          <Route path='login' element={<Login/>}/>
+          <Route path='register' element={<Register/>}/>
+          <Route path='forgot/:id' element={<Forgot/>}/>
+          <Route path='confirm/:token' element={<Confirm/>}/>
+          <Route path='reset/:token' element={<Reset/>}/>
+          <Route path='*' element={<NotFound />} />
         </Route>
 
 
-        <Route path='dashboard/' element={
+        <Route path='dashboard/*' element={
             <ProtectedRoute>
               <Routes>
                 <Route element={<Dashboard />}>

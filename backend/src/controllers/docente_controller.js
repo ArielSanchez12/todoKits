51143a79262adf361867d044efff07847d54a462
ArielSanchez12 <riveraariel433@gmail.com>
2025-07-14 19:17:04 -0500

    funcionalidades semana libre

--- backend/src/controllers/docente_controller.js
+++ backend/src/controllers/docente_controller.js
@@ -2,6 +2,7 @@ import docente from "../models/docente.js"
 import { sendMailToDocente } from "../config/nodemailer.js"
 import { v2 as cloudinary } from 'cloudinary'
 import fs from "fs-extra"
+import mongoose from "mongoose"
 
 const registrarDocente = async (req,res) => {
   //Obtener los datos
@@ -41,11 +42,48 @@ const registrarDocente = async (req,res) => {
 }
 
 const listarDocentes = async (req,res) => {
-    const docentes = await docente.find({statusDocente:true}).where('admin').equals(req.adminEmailBDD).select("-salida -createdAt -updatedAt -__v").populate('admin','_id nombre apellido')
-    res.status(200).json(docentes)
+  const docentes = await docente.find({statusDocente:true}).where('admin').equals(req.adminEmailBDD).select("-salida -createdAt -updatedAt -__v").populate('admin','_id nombre apellido')
+  res.status(200).json(docentes)
+}
+
+const detalleDocente = async(req,res)=>{
+  const {id} = req.params
+  if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el docente ${id}`});
+  const docentes = await docente.findById(id).select("-createdAt -updatedAt -__v").populate('admin','_id nombre apellido')
+  res.status(200).json(docentes)
+}
+
+const eliminarDocente = async (req,res)=>{
+  const {id} = req.params
+  if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+  if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el docente ${id}`})
+  const {salidaDocente} = req.body
+  await docente.findByIdAndUpdate(req.params.id,{salidaDocente:Date.parse(salidaDocente),statusDocente:false})
+  res.status(200).json({msg:"Fecha de salida del docente registrada exitosamente"})
+}
+
+const actualizarDocente = async(req,res)=>{
+    const {id} = req.params
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el docente ${id}`})
+    if (req.files?.imagen) {
+        const docentes = await docente.findById(id)
+        if (docentes.avatarMascotaID) {
+            await cloudinary.uploader.destroy(docentes.avatarMascotaID);
+        }
+        const cloudiResponse = await cloudinary.uploader.upload(req.files.imagen.tempFilePath, { folder: 'Docentes' });
+        req.body.avatarDocente = cloudiResponse.secure_url;
+        req.body.avatarDocenteID = cloudiResponse.public_id;
+        await fs.unlink(req.files.imagen.tempFilePath);
+    }
+    await docente.findByIdAndUpdate(id, req.body, { new: true })
+    res.status(200).json({msg:"Actualización exitosa del docente"})
 }
 
 export {
   registrarDocente,
-  listarDocentes
+  listarDocentes,
+  detalleDocente,
+  eliminarDocente,
+  actualizarDocente
 }

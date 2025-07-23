c1a33217c5e10f1443c0240e2ddb57832e134a8b
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-07-23 15:25:17 -0500

    TODO DE VUELTA DE CHAT BACK Y FRONT, PUNTO DE PAGOS FUNCIONAL

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,9 @@
 import connection from './database.js'
 import app from './server.js'
-import http from 'http'
-import { Server } from 'socket.io'
 
-// Conectar a la base de datos
+
 connection()
 
-// Crear servidor HTTP a partir de la app de Express
-const server = http.createServer(app)
-
-// Configurar Socket.io con CORS para local y producción
-const io = new Server(server, {
-    cors: {
-        origin: [
-            "http://localhost:5173", // frontend local
-            "https://kitsfrontend-zeta.vercel.app/", // reemplaza por tu dominio real de producción
-        ],
-        methods: ["GET", "POST"]
-    }
-})
-
-// Eventos de Socket.io
-io.on('connection', (socket) => {
-    console.log('Usuario conectado', socket.id)
-    socket.on('enviar-mensaje-front-back', (payload) => {
-        socket.broadcast.emit('enviar-mensaje-front-back', payload)
-    })
-})
-
-// Escuchar en el puerto definido en la app
-const PORT = app.get('port') || 3000
-server.listen(PORT, () => {
-    console.log(`Server ok on http://localhost:${PORT}`)
+app.listen(app.get('port'), () => { //Esta escuchando a nuestra variable global 'port'
+    console.log("Server ok")
 })

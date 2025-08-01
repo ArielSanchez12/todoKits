a3e9c1e3b4011e94b73fb0b6841544a317821231
ArielSanchez12 <riveraariel433@gmail.com>
2025-08-01 16:25:21 -0500

    actualizada la callbackURL para llamar al backend

--- backend/src/config/google.js
+++ backend/src/config/google.js
@@ -1,24 +1,20 @@
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import { use, serializeUser, deserializeUser } from 'passport';
+import passport from 'passport';
 
-// Aquí puedes usar tu modelo de usuario si deseas guardar los datos en la BDD
-// const Admin = require('../models/admin'); // Ejemplo
-
-use(new GoogleStrategy({
+passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://kitsbackend.vercel.app/auth/google/callback"
 },
 (accessToken, refreshToken, profile, done) => {
-  // Aquí puedes guardar el perfil en la base de datos si deseas
   console.log("Perfil de Google:", profile);
   return done(null, profile);
 }));
 
-serializeUser((user, done) => {
+passport.serializeUser((user, done) => {
   done(null, user);
 });
 
-deserializeUser((user, done) => {
+passport.deserializeUser((user, done) => {
   done(null, user);
 });

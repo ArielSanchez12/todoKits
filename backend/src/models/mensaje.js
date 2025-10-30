3e5b6fde7f7170dcaf69011708d934799f433bdb
ArielSanchez12 <riveraariel433@gmail.com>
2025-10-29 21:42:02 -0500

    agregue TTL (Time To Live) en el modelo mensajes para que los documentos se eliminen cada 10 horas asi amanece limpio el chat cada dia

--- backend/src/models/mensaje.js
+++ backend/src/models/mensaje.js
@@ -19,6 +19,12 @@ const mensajeSchema = new mongoose.Schema(
       recursos: [{ type: String }], // Cada elemento encriptado
       docenteOrigen: { type: String }, // Encriptado
     },
+    // ✅ NUEVO: Campo para TTL (expira a los 24 horas)
+    createdAtTTL: {
+      type: Date,
+      default: Date.now,
+      expires: 36000 // 36000 segundos = 10 horas
+    }
   },
   {
     timestamps: true,
@@ -26,6 +32,9 @@ const mensajeSchema = new mongoose.Schema(
     toObject: { virtuals: true }
   }
 );
+
+// ✅ Crear índice TTL (10 horas)
+mensajeSchema.index({ createdAtTTL: 1 }, { expireAfterSeconds: 36000 });
 
 // ✅ HOOK: Encriptar antes de guardar
 mensajeSchema.pre("save", function (next) {

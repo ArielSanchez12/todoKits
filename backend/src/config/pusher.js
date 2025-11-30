0f1c2816186f92f199b0863ba4944f9b1251b040
ArielSanchez12 <riveraariel433@gmail.com>
2025-11-30 15:29:37 -0500

    volví a poner a la config de pusher para entornos de pruebas ya que si se queda en el repo

--- backend/src/config/pusher.js
+++ backend/src/config/pusher.js
@@ -1,11 +1,23 @@
 import Pusher from "pusher";
 
-const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_KEY,
-  secret: process.env.PUSHER_SECRET,
-  cluster: process.env.PUSHER_CLUSTER,
-  useTLS: true,
-});
+let pusher;
+
+// En entorno de test, exportar un stub para que no intente usar claves reales (si no se hace esto, los test de chats fallan)
+if (process.env.NODE_ENV === "test") {
+  pusher = {
+    trigger: async () => {
+      console.log("[TEST] pusher.trigger llamado");
+      return true;
+    },
+  };
+} else { // En otros entornos, inicializar Pusher con las credenciales reales
+  pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
+    cluster: process.env.PUSHER_CLUSTER,
+    useTLS: true,
+  });
+}
 
 export default pusher;
